6b5ad39f0c5f7b58d3b2f91350389fe968ae0249
Ferreira <126113813+Ferreirafc1133@users.noreply.github.com>
2024-01-29 11:07:25 -0600

    estructura
    primeros pasos en la estructura del proyecto

--- API/index.js
+++ API/index.js
@@ -1,16 +1,19 @@
 const express = require('express');
-
 require('dotenv').config();
+const routes = require('./routes');
 
 const app = express();
 
-let port = process.env.PORT || 3000;
+app.use(routes);
+
+let port = process.env.PORT || 4000;
+
 
 app.listen(port, () => {
     if(process.env.NODE_ENV === 'dev'){
         console.log('listening on port: ' + port);
     }
     else{
-        console.log('listening disfuncion ereectil ');
+        console.log('listening...');
     }
-})
+});
