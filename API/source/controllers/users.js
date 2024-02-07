8fb6e931cf21e857d46715c2d73839f1d69fbf99
Ferreira <126113813+Ferreirafc1133@users.noreply.github.com>
2024-02-07 11:59:50 -0600

    middlewares
    esta clase estuvo esasy, basicamente puras casetas y modular todo en 1000000 archivos diferentes,
    importante el orden de las rutas importa

--- API/source/controllers/users.js
+++ API/source/controllers/users.js
@@ -5,6 +5,7 @@ const ResponseStatus = require("./../utils/response-status");
 class UsersController{
     getUsers(req, res){
         const user = new User();
+        console.log('user: ', req.user); 
         user.find().then(response => {
             res.send(response);
         }).catch(e => {
@@ -12,7 +13,25 @@ class UsersController{
         });
         console.log("here");
     }
-    createUsers(){}
+
+    createUsers(req, res){
+        res.send("Sexo ANAAAAL");
+    }
+
+    getUserId(req, res){
+        const userId = req.params.id
+        res.send('get userId: ' + userId);
+    }
+
+    getUserRole(req, res) {
+        if (req.user) {
+            res.send('Get user role: ' + req.user.role);
+            console.log("Entro a user role");
+        } else {
+            res.status(ResponseStatus.UNATHENTICATED).send('Usuario no autenticado');
+        }
+    }     
+
 }
 
 
--- API/source/routes/users.js
+++ API/source/routes/users.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
-const usersController = require('./../controllers/users');
+const {getUsers, createUsers, getUserId, getUserRole} = require('./../controllers/users'); 
+const { authMid, authRolMid } = require('./../middlewares/index');
 
+const rol = "admin";
 
-router.get('', usersController.getUsers);
-router.post('', usersController.createUsers);
+router.get('', authMid, getUsers);
+router.get('/role', authMid, authRolMid("admin"), getUserRole); 
+router.get('/new', authMid, createUsers);
+router.get('/id/:id', authMid, getUserId);
+
 
 module.exports = router;
