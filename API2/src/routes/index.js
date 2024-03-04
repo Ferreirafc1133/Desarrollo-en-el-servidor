963f24e49db0cf2028dc03c90f87b5638b17f434
Ferreira <jose.chavez@iteso.mx>
2024-03-04 10:48:54 -0600

    handlebars y estilos

--- API2/src/routes/index.js
+++ API2/src/routes/index.js
@@ -1,9 +1,35 @@
 const router = require('express').Router();
 const path = require('path');
+const axios = require('axios');
 
 router.get('', (req, res) => {
-    const url = path.join(__dirname, '..', 'views', 'index.html');
-    res.sendFile(url);
+    //const url = path.join(__dirname, '..', 'views', 'index.html');
+    //res.sendFile(url);
+    res.render('home', {
+        title: 'titulo dinamique' 
+    });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about',{
+        layout: 'main-login'
+    });
+});
+
+
+router.get('/tareas', (req, res) => {
+    const userId = req.query.user;
+    if(userId){
+        const url = `https://jsonplaceholder.typicode.com/todos?userId=${userId}`;
+        axios.get(url).then((response) => {
+            res.render('todos', {
+                userId,
+                todos: response.data
+            });
+        });
+    } else{
+        res.render('todos');
+    }
 });
 
 module.exports = router;
